659d9b95a6201be92ae840329c2053685eaa6f9c
Cmoo128 <78281065+Cmoo128@users.noreply.github.com>
2022-01-25 08:56:33 -0600

    Update sketch.js

--- HW1/Example_1/sketch.js
+++ HW1/Example_1/sketch.js
@@ -1,12 +1,33 @@
 function setup() {
-  createCanvas(465, 250);
+  createCanvas(400, 400);
 }
 
+
 function draw() {
-  background('#00ff00');
   
-  circle(120, 125, 200);
+  background("#00008B");
   
-  square(260, 35, 175);
+  noStroke();
+  fill("#FFFFFF")
+  circle(200, 200, 185);
+
+  fill(0, 150, 0);
+  circle(200, 200, 175);
+  
+  fill("#FFFFFF")
+  beginShape();
+  vertex(200, 100); 
+  vertex(230,170); 
+  vertex(293,175); 
+  vertex(245, 215);
+  vertex(265, 275);
+    vertex(200, 240); 
+  vertex(135, 275);
+  vertex(150, 215);
+  vertex(107,175);
+  vertex(170,170); 
+  endShape();
+
+
   
 }
