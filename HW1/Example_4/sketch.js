367c2293116a0c831c80f7fbee833f2a9a6841d9
Cmoo128 <78281065+Cmoo128@users.noreply.github.com>
2022-01-25 10:24:52 -0600

    Update sketch.js

--- HW1/Example_4/sketch.js
+++ HW1/Example_4/sketch.js
@@ -14,16 +14,16 @@ function draw() {
   fill(0, 150, 0);
   circle(200, 200, 175);
   
-  fill("#FFFFFF")
+ fill("#FFFFFF")
   beginShape();
   vertex(200, 102); 
   vertex(230,165); 
   vertex(293,165); 
-  vertex(250, 215);
+  vertex(240, 215);
   vertex(265, 275);
     vertex(200, 235);
   vertex(135, 275);
-  vertex(150, 215);
+  vertex(160, 215);
   vertex(107,165); 
   vertex(170,165); 
   endShape();
@@ -34,11 +34,11 @@ function draw() {
   vertex(200, 110); 
   vertex(225,170); 
   vertex(285,170); 
-  vertex(243, 215);
-  vertex(259, 268);
+  vertex(236, 212);
+  vertex(258, 267);
     vertex(200, 228);
-  vertex(200-59, 268);
-  vertex(200-43, 215);
+  vertex(200-58, 267);
+  vertex(200-36, 212);
   vertex(200-85,170); 
   vertex(200-25,170); 
   endShape();
